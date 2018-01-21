0564a310f193d99d35f0d2527d04bd2725568cc4
Matt Usifer <mattusifer@gmail.com>
2018-01-21 11:26:25 -0500

    Refresh directory test, bug fix

--- tramps3-mode.el
+++ tramps3-mode.el
@@ -136,11 +136,11 @@ The original function and arguments are available as ORIG-DIRED-DO-COPY and ARGS
   "A wrapper around dired's ‘dired-find-file’ function.
 The original function and arguments are available as ORIG-DIRED-FIND-FILE and ARGS."
   (if (is-tramps3-mode-active)
-      (let* ((current-local-file (dired-get-filename))
-             (tramps3-is-directory (tramps3-local-path-to-s3-path current-local-file)))
+      (let* ((current-local-file (dired-get-filename)))
         (if (tramps3-is-directory current-local-file)
             (progn
               (apply orig-dired-find-file args)
+              (tramps3-mode)
               (revert-buffer t t))
           (progn
             (tramps3-s3-cp (tramps3-local-path-to-s3-path current-local-file) current-local-file)
