dd622a328f93942ceefa1a7318ae9d1124970436
Matt Usifer <mattusifer@gmail.com>
2018-02-04 08:12:18 -0500

    Better testing

--- test/tramps3-tests.el
+++ test/tramps3-tests.el
@@ -13,6 +13,13 @@
          ,@body)
        (delete-directory ,dir t))))
 
+(defmacro tramps3-setup-teardown-aws-test-dir (&rest body)
+  (let ((aws-test-dir (make-symbol "aws-test-dir")))
+    `(let ((,aws-test-dir "/tmp/tramps3-aws-testing"))
+       (when (file-exists-p ,aws-test-dir) (delete-directory ,aws-test-dir t))
+       ,@body
+       (when (file-exists-p ,aws-test-dir) (delete-directory ,aws-test-dir t)))))
+
 (ert-deftest tramps3-string-starts-with ()
   (should (tramps3-string-starts-with "string" "s"))
   (should (not (tramps3-string-starts-with "xstring" "s"))))
@@ -29,22 +36,60 @@
 (require 'tramps3-io)
 
 (ert-deftest tramps3-s3-ls-test ()
-  (let* ((inhibit-message t)
-         (res (tramps3-s3-ls "s3://tramps3/")))
-    (should (equal res '("test/" "testdir/" "testfile")))))
+  (tramps3-setup-teardown-aws-test-dir
+   (let* ((inhibit-message t)
+          (res (tramps3-s3-ls "s3://tramps3/")))
+     (should (equal res '("test/" "testdir/" "testfile"))))))
+
+(ert-deftest tramps3-s3-mv-test ()
+  (tramps3-setup-teardown-aws-test-dir
+   (let* ((inhibit-message t))
+     (tramps3-s3-mv "s3://tramps3/testdir/" "s3://tramps3/testdir-renamed/" t)
+     (tramps3-s3-mv "s3://tramps3/testfile" "s3://tramps3/testfile-renamed")
+     (should (equal (tramps3-s3-ls "s3://tramps3/") '("test/" "testdir-renamed/" "testfile-renamed"))))))
+
+(ert-deftest tramps3-s3-cp-test ()
+  (tramps3-setup-teardown-aws-test-dir
+   (let* ((inhibit-message t))
+     (tramps3-s3-cp "s3://tramps3/testdir/" "s3://tramps3/testdir-copied/" t)
+     (tramps3-s3-cp "s3://tramps3/testfile" "s3://tramps3/testfile-copied")
+     (should (equal (tramps3-s3-ls "s3://tramps3/") '("test/" "testdir-copied/" "testdir/"
+                                                      "testfile" "testfile-copied"))))))
+
+(ert-deftest tramps3-s3-rm-test ()
+  (tramps3-setup-teardown-aws-test-dir
+   (let* ((inhibit-message t))
+     (tramps3-s3-rm "s3://tramps3/testdir/" t)
+     (tramps3-s3-rm "s3://tramps3/testfile" t)
+     (should (equal (tramps3-s3-ls "s3://tramps3/") '("test/"))))))
 
 (ert-deftest tramps3-refresh-tmp-dir-test ()
+  (tramps3-mode)
   (tramps3-setup-teardown-test-dir
    (let* ((inhibit-message t)
           (tramps3-tmp-s3-dir (substring tramps3-test-directory 0 -1))
-          (tramps3-subdir (format "%s/tramps3/" tramps3-tmp-s3-dir)))
-     (tramps3-refresh-tmp-dir tramps3-subdir)
+          (tramps3-subdir (format "%s/tramps3/" tramps3-tmp-s3-dir))
+          (tramps3-subdir2 (format "%s/another-bucket/" tramps3-tmp-s3-dir)))
+     (make-directory tramps3-subdir t)
+     (make-directory tramps3-subdir2 t)
+     (dired tramps3-subdir) ;; background tramps3 buffer
+     (dired tramps3-subdir2) ;; active buffer
+     (tramps3-refresh-tmp-dir)
+
+     ;; background buffer dir should exist, but not be refreshed
      (let ((organized-file-list (--separate (tramps3-is-directory
                                              (format "%s%s" tramps3-subdir it))
                                             (directory-files tramps3-subdir))))
-       (should (and (member "test" (car organized-file-list))
-                    (member "testdir" (car organized-file-list))))
-       (should (and (member "testfile" (car (-take-last 1 organized-file-list)))))))))
+       (should (eq 2 (length (car organized-file-list))))
+       (should (eq 0 (length (car (-take-last 1 organized-file-list))))))
+
+     ;; active buffer dir should be fully refreshed
+     (let ((organized-file-list (--separate (tramps3-is-directory
+                                             (format "%s%s" tramps3-subdir2 it))
+                                            (directory-files tramps3-subdir2))))
+       (should (and (member "dir-in-another-bucket" (car organized-file-list))
+                    (member "dir2-in-another-bucket" (car organized-file-list))))
+       (should (member "tramps3" (car (-take-last 1 organized-file-list))))))))
 
 (ert-deftest tramps3-mkdirs-test ()
   (tramps3-setup-teardown-test-dir
